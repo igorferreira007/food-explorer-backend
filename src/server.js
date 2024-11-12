37b64a320e387c80e4ffed4a93600f3e4a08ed50
igorferreira007 <ferreiraigor007@gmail.com>
2024-11-12 14:38:17 -0300

    Adicionado a url do frontend no Cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173/"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173/", "https://food-3xplorer.netlify.app"],
   credentials: true
 }))
 
